6f103291f8dd9acaf2e9e1f55738b300eabcec8e
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-11-05 04:40:27 -0300

    Updates

---
+++ precache-manifest.450cffba891a4127e40d07fb6cd8a3bf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf071a0e462573a53c1480c5d5493b9d",
+    "url": "/sd-03-trybeer-3/index.html"
+  },
+  {
+    "revision": "49246ea87d431749a1c0",
+    "url": "/sd-03-trybeer-3/static/css/main.71674b4d.chunk.css"
+  },
+  {
+    "revision": "9e8ebbad3e2a2f655113",
+    "url": "/sd-03-trybeer-3/static/js/2.808d57a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sd-03-trybeer-3/static/js/2.808d57a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49246ea87d431749a1c0",
+    "url": "/sd-03-trybeer-3/static/js/main.dba57350.chunk.js"
+  },
+  {
+    "revision": "0c56084c98ccf4a8c3ab",
+    "url": "/sd-03-trybeer-3/static/js/runtime-main.f9e01c36.js"
+  },
+  {
+    "revision": "170a14fd3ee7dbd7308582502c710dd9",
+    "url": "/sd-03-trybeer-3/static/media/bigBangBeerLogo.170a14fd.gif"
+  },
+  {
+    "revision": "de8cb2484f1e2cd94b36a78ef74dc672",
+    "url": "/sd-03-trybeer-3/static/media/cardapio.de8cb248.svg"
+  },
+  {
+    "revision": "dc52dd2e9fc0af7f3f9fd282147c42ba",
+    "url": "/sd-03-trybeer-3/static/media/tableslogan.dc52dd2e.jpg"
+  }
+]);
